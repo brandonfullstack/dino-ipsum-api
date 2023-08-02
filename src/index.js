c71d12eebbb996f9376f1492a4489a49bdaa1109
Brandon Wright <brandonfullstack@gmail.com>
2023-08-01 23:53:53 -0400

    Add Dino Ipsum API logic

--- src/index.js
+++ src/index.js
@@ -2,18 +2,40 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-window.addEventListener("load", function () {
-  console.log("project Template");
+function getDinos(words, paragraphs) {
+  let request = new XMLHttpRequest();
+  const url = `https://dinoipsum.com/api/?format=json&paragraphs=${paragraphs}&words=${words}`;
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response);
+    }
+  });
+
+  request.addEventListener("readystatechange", function() {
+    console.log(this.readyState);
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+// UI Logic
+
+function printElements(response) {
+  document.querySelector('#dino-container').innerHTML = response;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const words = document.querySelector('#words').value;
+  const paragraphs = document.querySelector('#paragraphs').value;
+  document.querySelector('#words').value = null;
+  document.querySelector('#paragraphs').value = null;
+  getDinos(words, paragraphs);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
-
-var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://dinoipsum.com/api/?format=html&words=10&paragraphs=3');
-xhr.onload = function() {
-  if (this.status === 200) {
-    document.querySelector('#dino-container').innerHTML = this.response;
-  } else {
-    console.log('Where did all the dinosaurs go?');
-  }
-};
-xhr.send();
